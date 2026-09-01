f3b87ff60973eb8595a095deb6ba62c859131ca9
Prepward <prepward@gmail.com>
2026-09-01 20:55:17 +0530

    Add AboutUs component

---
+++ src/AboutUs.jsx
@@ -0,0 +1,21 @@
+function AboutUs() {
+  return (
+    <section className="about-us">
+      <h2>About Paradise Nursery</h2>
+
+      <p>
+        Paradise Nursery is a houseplant shopping destination dedicated to
+        bringing the beauty of nature into your home. We offer a carefully
+        selected collection of indoor plants suitable for homes, offices,
+        and plant lovers of all experience levels.
+      </p>
+
+      <p>
+        Our goal is to make it simple and enjoyable to discover beautiful
+        plants and create a greener, healthier, and more peaceful living space.
+      </p>
+    </section>
+  );
+}
+
+export default AboutUs;
